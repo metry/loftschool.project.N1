a783d27f6cd43a2ce969b99bb42e4dca06519a1d
metry <fortrun.php@gmail.com>
2018-06-24 20:39:15 +0300

    #0001 add project N1

---
+++ app/Core/View.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Core;
+
+class View
+{
+    private $loader;
+    private $twig;
+
+    public function renderTemplate(string $templateView, $data = [])
+    {
+        $template = APPLICATION_PATH . "Views/" . $templateView . ".php";
+        try {
+            if (!file_exists($template)) {
+                throw new \Exception("Шаблон отсутсвует");
+            } else {
+                extract($data); // импортируем переменные из массива в текущую символьную таблицу
+                require_once $template;
+            }
+        } catch (\Exception $e) {
+            require APPLICATION_PATH . "errors/showError404.php";
+        }
+    }
+
+    public function __construct()
+    {
+        $this->loader = new \Twig_Loader_Filesystem(APPLICATION_PATH . "Views");
+        $this->twig = new \Twig_Environment($this->loader);
+    }
+
+    public function twigLoad(String $templateView, $data = [])
+    {
+        echo $this->twig->render($templateView.".twig", $data);
+    }
+}
---
+++ app/Core/config.example.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Приложение находится в режиме логгирования ошибок
+ * Для отключения логирования, необходимо закомментировать строку ниже
+ */
+const APPLICATION_TYPE = 'log';
+
+const HOST = 'localhost';
+const DBNAME = 'database';
+const USER = 'root';
+const PASS = 'password';
+
+// Настройки для отправки писем через SMTP-сервер
+
+const EMAIL = 'test@example.com';
+const USER_NAME = 'test';
+const USER_PASSWORD = 'password';
+
+const SMTP_HOST = 'smtp.example.com';
+const SMTP_PORT = '465';
+const SMTP_SECURITY = 'SSL';
